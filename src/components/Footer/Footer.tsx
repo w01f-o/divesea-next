a4211a798d66efd9384cd1e8df28ca40ff203623
w01f <arsenimarchenko@bk.ru>
2024-05-23 15:07:16 +0300

    toggle switch

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,12 @@
 import { FC } from "react";
 import { Container, Row } from "@w01f-o/react-grid-layout";
 import footerStyles from "./footer.module.scss";
-import ToggleSwitch from "@/components/UI/ToggleSwitch/ToggleSwitch";
 
 const Footer: FC = () => {
   return (
     <footer className={footerStyles.footer}>
       <Container>
-        <Row>
-          <ToggleSwitch />
-        </Row>
+        <Row></Row>
       </Container>
     </footer>
   );
