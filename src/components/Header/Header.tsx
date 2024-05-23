553eece6636744ced0feec6a268969ba8e046d32
w01f <arsenimarchenko@bk.ru>
2024-05-23 11:43:46 +0300

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,8 @@ import headerStyles from "./header.module.scss";
 import Link from "next/link";
 import LogoIcon from "@/components/UI/Icons/LogoIcon";
 import NavBar from "@/components/Header/NavBar/NavBar";
-import Input from "@/components/UI/Input/Input";
-import SearchIcon from "@/components/UI/Icons/SearchIcon";
 import Button from "@/components/UI/Button/Button";
+import Search from "@/components/Header/Search";
 
 const Header: FC = () => {
   return (
@@ -22,11 +21,7 @@ const Header: FC = () => {
             <NavBar />
           </Col>
           <Col xxl={4}>
-            <Input
-              type={"text"}
-              placeholder={"Search Art Work / Creator"}
-              icon={{ element: <SearchIcon />, position: "left" }}
-            />
+            <Search />
           </Col>
           <Col xxl={2}>
             <Button title={"Connect wallet"} background={"black"}>
---
+++ src/components/Header/Search.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ChangeEvent, FC, FormEvent, useState } from "react";
+import SearchIcon from "@/components/UI/Icons/SearchIcon";
+import Input from "@/components/UI/Input/Input";
+import { useRouter } from "next/navigation";
+
+const Search: FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const router = useRouter();
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const url = new URLSearchParams();
+    url.set("nft", inputValue);
+    url.set("creator", inputValue);
+
+    router.push(`/search?${url}`);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <Input
+        type={"text"}
+        placeholder={"Search Art Work / Creator"}
+        icon={{ element: <SearchIcon />, position: "left" }}
+        value={inputValue}
+        onChange={changeHandler}
+      />
+    </form>
+  );
+};
+
+export default Search;
